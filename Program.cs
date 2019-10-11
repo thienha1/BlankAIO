3e60965e1fe1bc5d5d0deccbccb67721d39b35b7
thienha1 <hathien69@gmail.com>
2019-10-11 23:08:14 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using EnsoulSharp;
+using EnsoulSharp.SDK;
+using System;
+using Notification = EnsoulSharp.SDK.MenuUI.Notification;
+using Notifications = EnsoulSharp.SDK.MenuUI.Notifications;
+
+namespace BlankAIO
+{
+    internal class Program
+    {
+        private static AIHeroClient Player => ObjectManager.Player;
+
+        private static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += GameEventOnOnGameLoad;
+        }
+
+        private static void GameEventOnOnGameLoad()
+
+        {
+            var notify = new Notification("Blank.AIO",
+                "Blank.AIO Loaded. \n \n");
+            Notifications.Add(notify);
+            switch (Player.CharacterName)
+            {
+                case "Leona":
+                    Champions.Leona.LeonaMain();
+                    break;
+
+                case "Pyke":
+                    Pyke.On_Load();
+                    break;
+
+            }
+        }
+    }
+}
