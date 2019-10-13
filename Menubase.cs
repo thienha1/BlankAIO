d335b0168cce9ce9d0b86c09b44504ce2d661743
thienha1 <hathien69@gmail.com>
2019-10-13 23:56:26 +0700

    Add files via upload

--- Menubase.cs
+++ Menubase.cs
@@ -5,12 +5,12 @@ namespace BlankAIO
     {
         public class Pyke_Combat
         {
-            public static readonly MenuBool Q = new MenuBool("q", "Use [Q]");
-            public static readonly MenuSlider Qhit = new MenuSlider("qhit", "^ Q Hitchance = 1-Low ~ 4-Very High", 3, 1, 4);
-            public static readonly MenuBool E = new MenuBool("e", "Use [E]");
-            public static readonly MenuBool Etower = new MenuBool("Etower", "^ Don't use if enemy is under turret");
-            public static readonly MenuBool R = new MenuBool("r", "Use [R]");
-            public static readonly MenuBool Rkill = new MenuBool("rkill", "^Only if Enemy is Killable");
+            public static readonly MenuBool QC = new MenuBool("qh", "Use [Q]");
+            public static readonly MenuSlider QhitC = new MenuSlider("qhit", "^ Q Hitchance = 1-Low ~ 4-Very High", 3, 1, 4);
+            public static readonly MenuBool EC = new MenuBool("e", "Use [E]");
+            public static readonly MenuBool EtowerC = new MenuBool("Etower", "^ Don't use if enemy is under turret");
+            public static readonly MenuBool RC = new MenuBool("r", "Use [R]");
+            public static readonly MenuBool RkillC = new MenuBool("rkill", "^Only if Enemy is Killable");
         }
         public class Pyke_Clear
         {
